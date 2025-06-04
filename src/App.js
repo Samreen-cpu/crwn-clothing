2a184a1d6a8a375f905de069b99fefb0bcf3089a
Samreen-cpu <samreenhabib52@gmail.com>
2025-06-04 15:14:44 +0500

    created checkout page along with checkout items and funcitionality of checkout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./routers/navigation/navigation.component"
 import Home from "./routers/home/home.component";
 import Authentication from "./routers/authentication/authentication.component";
 import Shop from "./routers/shop/shop.component";
+import CheckOut from "./routers/checkout/checkout.component";
 const App=()=>{
   return(
   <Routes>
@@ -11,6 +12,7 @@ const App=()=>{
      <Route index element={<Home/>}/>
      <Route path='shop' element={<Shop/>}/>
       <Route path='auth' element={<Authentication/>}/>
+        <Route path='checkout' element={<CheckOut/>}/>
     </Route>
   </Routes>
   );
